a05688a613534ccf6e088ea3caf0c3ad78406c43
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-04-21 21:25:13 -0700

    Fixed bcrypt password
    Was not awaiting password to generate before creating user

--- BackEnd/routes/jwtAuth.js
+++ BackEnd/routes/jwtAuth.js
@@ -16,12 +16,12 @@ router.post("/register", async (req, res) => {
         const saltRound = 10;
         const salt = await bcrypt.genSalt(saltRound)
 
-        const bcryptPassword = bcrypt.hash(password, salt)
+        const bcryptPassword = await bcrypt.hash(password, salt)
 
         const newUser = await pool.query("INSERT INTO users (username, password, first_name, last_name, email) VALUES ($1, $2, $3, $4, $5) RETURNING *",
         [username, bcryptPassword, first_name, last_name, email]);
 
-        const token = jwtGenerator(newUser.rows[0].user_id);
+        const token = jwtGenerator(newUser.rows[0].id);
 
         res.json({ token })
 
@@ -31,7 +31,34 @@ router.post("/register", async (req, res) => {
     }
 });
 
+//login route
 
+router.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await pool.query("SELECT * FROM users WHERE username = $1", [username])
+
+        if (user.rows.length === 0) {
+            return res.status(401).send("Username or Password is incorrect")
+
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+
+        if (!validPassword) {
+            return res.status(401).json("Username or Password is incorrect")
+        }
+
+        const token = jwtGenerator(user.rows[0].username);
+
+        res.json({ token })
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+})
 
 
 module.exports = router;
