4e6c3aa8079dbf27075a5c96232a660659af2fe7
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-04-23 18:36:41 -0700

    Added logout button

--- FrontEnd/src/Components/Home.js
+++ FrontEnd/src/Components/Home.js
@@ -1,9 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
-const Home = () => {
+const Home = ({ setAuth }) => {
+
+
+    const logout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        setAuth(false);
+    }
     return (
         <Fragment>
             <h1>Welcome to the Taylor Winfrey Payment Experience</h1>
+            <button className="btn btn-success" onClick={e => logout(e)}>Logout</ button>
         </Fragment>
     );
 };
