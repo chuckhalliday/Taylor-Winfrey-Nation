aba7f21633d60cb9751bde3e643f60b53945827f
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-05-11 00:05:01 -0700

    Product details layout tweaks

--- FrontEnd/src/Components/ProductDetails.js
+++ FrontEnd/src/Components/ProductDetails.js
@@ -31,19 +31,18 @@ function ProductDetails(props) {
                   alt={product.name}
                 ></img>
               </div>
-              <div className="col-1">
+              <div>
                 <ul>
                   <li>
                     <h1>{product.name}</h1>
                   </li>
-                  <li>Price : {product.price}</li>
                   <li>
                     Description:
                     <p>{product.description}</p>
                   </li>
                 </ul>
               </div>
-              <div className="col-1">
+              <div>
                 <div className="card card-body">
                   <ul>
                     <li>
@@ -54,7 +53,7 @@ function ProductDetails(props) {
                     </li>
                     <li>
                       <div className="row">
-                        <div>Status</div>
+                        <div>Status:</div>
                         <div>
                           {product.countInStock > 0 ? (
                             <span className="success">In Stock</span>
