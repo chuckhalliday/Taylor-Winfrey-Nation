8de1d32a0a8ab4937ea5e661abd46edf6a5048cb
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-06-30 19:56:11 -0700

    Test

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 const path = require("path");
 const pool = require('./routes/queries');
 
