3f6be25961e98409a72c5f3bd0899da0cf793846
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-07-02 16:55:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({
     extended: true,})
 );
 
+app.use(express.static(__dirname + '/'));
 app.use(express.static(publicPath));
 
 if (process.env.NODE_ENV === "production") {
